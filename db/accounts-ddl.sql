1629c25eca2d8ee0e284eca48459eae734ee701d
James Markijohn <120273527+jamesmarkijohn@users.noreply.github.com>
2026-09-24 19:58:41 -0400

    DDL for accounts database

---
+++ db/accounts-ddl.sql
@@ -0,0 +1,11 @@
+-- Data Definition Language (DDL) for accounts db
+CREATE TABLE accounts (
+    username VARCHAR(50) PRIMARY KEY,
+    email VARCHAR(254) DEFAULT NULL
+    password VARCHAR(128) NOT NULL,
+    full_name VARCHAR(100) DEFAULT NULL,
+    phone_number INTEGER(15) DEFAULT NULL
+    created DATE
+);
+
+-- will add MFA fields later
