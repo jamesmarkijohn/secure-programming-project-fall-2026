759c1384cae0bbc1e3957bcba4561aa9c674c0ef
James Markijohn <120273527+jamesmarkijohn@users.noreply.github.com>
2026-09-25 10:45:48 -0400

    Adding foreign key constraint for user_id field

--- db/security-ddl.sql
+++ db/security-ddl.sql
@@ -2,7 +2,10 @@
 CREATE TABLE security (
     id INT AUTO_INCREMENT PRIMARY KEY,
     event_time TIMESTAMP NOT NULL,
-    username VARCHAR(50) NOT NULL,
+    user_id INTEGER NOT NULL,
     event_type VARCHAR(50) NOT NULL,
-    event_desc VARCHAR(255) DEFAULT NULL
+    event_desc VARCHAR(255) DEFAULT NULL,
+    CONSTRAINT fk_user
+    FOREIGN KEY (user_id)
+    REFERENCES accounts(user_id)
 );
