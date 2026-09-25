69bd38cdca974ebc10e764314d9946249e4951a7
James Markijohn <120273527+jamesmarkijohn@users.noreply.github.com>
2026-09-25 10:45:04 -0400

    Adding foreign key constraint for user_id field

--- db/movement-ddl.sql
+++ db/movement-ddl.sql
@@ -1,8 +1,11 @@
 -- Data Definition Language (DDL) for movement db
 CREATE TABLE movement (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
+    user_id INTEGER NOT NULL,
     start_room VARCHAR(50) NOT NULL,
     end_room VARCHAR(50) NOT NULL,
-    event_time TIMESTAMP NOT NULL
+    event_time TIMESTAMP NOT NULL,
+    CONSTRAINT fk_user
+    FOREIGN KEY (user_id)
+    REFERENCES accounts(user_id)
 );
