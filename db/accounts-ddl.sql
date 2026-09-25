4297e0c073a3bad3fff39ffe2fdcfc813cdadf69
James Markijohn <120273527+jamesmarkijohn@users.noreply.github.com>
2026-09-25 09:32:24 -0400

    Fixing syntax errors (missing commas)

--- db/accounts-ddl.sql
+++ db/accounts-ddl.sql
@@ -1,10 +1,10 @@
 -- Data Definition Language (DDL) for accounts db
 CREATE TABLE accounts (
     username VARCHAR(50) PRIMARY KEY,
-    email VARCHAR(254) DEFAULT NULL
+    email VARCHAR(254) DEFAULT NULL,
     password VARCHAR(128) NOT NULL,
     full_name VARCHAR(100) DEFAULT NULL,
-    phone_number INTEGER(15) DEFAULT NULL
+    phone_number INTEGER(15) DEFAULT NULL,
     created DATE
 );
 
